8d56ac56c87c5f30ad9114b7e74f67c1dcd0e38d
kanadeiar <kanadeiar@gmail.com>
2023-04-11 22:17:15 +0300

    Добавлено редактирование товаров

--- JulietsStore.App/Abstracts/IProductsRepo.cs
+++ JulietsStore.App/Abstracts/IProductsRepo.cs
@@ -3,4 +3,7 @@ namespace JulietsStore.App.Abstracts;
 public interface IProductsRepo
 {
     IQueryable<Product> Products { get; }
+    void CreateProduct(Product product);
+    void UpdateProduct(Product product);
+    void DeleteProduct(Product product);
 }
--- JulietsStore.Infra/Data/ProductsRepo.cs
+++ JulietsStore.Infra/Data/ProductsRepo.cs
@@ -8,4 +8,20 @@ public class ProductsRepo : IProductsRepo
         _context = context;
     }
     public IQueryable<Product> Products => _context.Set<Product>();
+    public void CreateProduct(Product product)
+    {
+        _context.Add(product);
+        _context.SaveChanges();
+    }
+
+    public void UpdateProduct(Product product)
+    {
+        _context.SaveChanges();
+    }
+
+    public void DeleteProduct(Product product)
+    {
+        _context.Remove(product);
+        _context.SaveChanges();
+    }
 }
