808315d5e3cfe3cba139be25d51642f5714120de
kanadeiar <kanadeiar@gmail.com>
2023-04-11 23:05:07 +0300

    Ускорение развертыванияДобавление авторизации

---
+++ JulietsStore.Domain/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JulietsStore.Domain.ViewModels;
+
+public class LoginViewModel
+{
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? Password { get; set; }
+    public string ReturnUrl { get; set; } = "/";
+}
+
